9db779d4b50fecf110799ea81782c12493136dbb
earn <arputsada25@gmail.com>
2023-04-03 10:42:05 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿class Plogram{
+    static void Main(string[] args){
+        
+
+        static int Max (int number , int End) {
+            if ( numbwr != End){
+                return number ;
+                
+            }
+            return End
+        }
+        static void Main(string[] args){
+            int result;
+
+        }
+        Console.write( " please input number:" );
+        string result password = console.ReadLine();
+
+        Console.write( " please input (End):");
+        string word password = console.ReadLine();
+
+        strinr FindMax = Console.ReadLine();
+        strinr FindMin = Console.ReadLine();
+        strinr FindMin = Console.ReadLine();
+        int swapRound = int.Parse(Console.ReadLine());
+        
+            
+        }
+
+        Console.WriteLine(FindMax);
+         Console.WriteLine(FindMin);
+          Console.WriteLine(FindMin);
+
+    }
+    static void Swap(ref string number, ref string End) {
+        string tmp = a;
+        a = b;
+        b = tmp;
+        }
+
+    
